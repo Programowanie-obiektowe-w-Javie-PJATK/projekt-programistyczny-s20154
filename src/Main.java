c9be8f65ee539c7eed271d068d80816d3b9dd930
Kijańczuk, Damian <s20154@pjwstk.edu.pl>
2020-06-09 23:04:55 +0200

    Extend Main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 public class Main implements ActionListener, KeyListener {
     public static final int WINDOW_WIDTH  = 640;
     public static final int WINDOW_HEIGHT = 480;
-    private JFrame frame;
-    private JPanel panel;
-    private Timer tick;
+    private JFrame  frame;
+    private JPanel  panel;
+    private Timer   tick;
+    private Boolean isPaused;
 
     private Bird bird;
+    private ArrayList<Rectangle> pipes;
+    private int score;
 
+    public Main(){
+        frame = new JFrame("Flappy Bird the Game");
+        bird  = new Bird();
+        pipes = new ArrayList<Rectangle>();
+        panel = new Overlay(this, bird, pipes);
+        tick  = new Timer(100, this);
+        isPaused = true;
+        score = 0;
+
+        frame.add(panel);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setVisible(true);
+        frame.addKeyListener(this);
+
+        tick.start();
+    }
 
     public static void main(){
 
@@ -20,12 +41,22 @@ public class Main implements ActionListener, KeyListener {
 
     @Override
     public void actionPerformed(ActionEvent a){
+        panel.repaint();
+        if(isPaused == true)
+            return;
+
+        bird.pullDown();
 
     }
 
     @Override
     public void keyPressed(KeyEvent keyEvent) {
+        switch (keyEvent.getKeyCode()){
+            case KeyEvent.VK_SPACE:
+                bird.jump(); break;
 
+
+        }
     }
 
 
