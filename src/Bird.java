7852ec107fb5d142ac000b04c40703541fae4013
Kijańczuk, Damian <s20154@pjwstk.edu.pl>
2020-06-09 22:30:16 +0200

    Make main project classes

---
+++ src/Bird.java
@@ -0,0 +1,28 @@
+
+private interface Player {
+    void jump();
+    void pullDown();
+    void reset();
+}
+
+public class Bird implements Player {
+    private double x, y;
+    private double Vx, Vy;
+
+    public Bird(){
+
+    }
+
+    public void jump(){
+
+    }
+
+    public void pullDown(){
+
+    }
+
+    public void reset(){
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Main implements ActionListener, KeyListener {
+    public static final int WINDOW_WIDTH  = 640;
+    public static final int WINDOW_HEIGHT = 480;
+    private JFrame frame;
+    private JPanel panel;
+    private Timer tick;
+
+    private Bird bird;
 
 
+    public static void main(){
 
-public class Main {
+    }
 
-    
+    @Override
+    public void actionPerformed(ActionEvent a){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+
 }
